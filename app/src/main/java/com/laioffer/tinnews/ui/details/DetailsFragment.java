56f53fb3fde6c95defae87ad280371e5655ed18d
sarinayang <78892067+sarinayang@users.noreply.github.com>
2021-12-30 23:12:37 -0500

    Initial Commit

---
+++ app/src/main/java/com/laioffer/tinnews/ui/details/DetailsFragment.java
@@ -0,0 +1,63 @@
+package com.laioffer.tinnews.ui.details;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.databinding.FragmentDetailsBinding;
+import com.laioffer.tinnews.model.Article;
+import com.squareup.picasso.Picasso;
+
+
+public class DetailsFragment extends Fragment {
+
+    private FragmentDetailsBinding binding;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public DetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Article article = DetailsFragmentArgs.fromBundle(getArguments()).getArticle();
+        binding.detailsTitleTextView.setText(article.title);
+        binding.detailsAuthorTextView.setText(article.author);
+        binding.detailsDateTextView.setText(article.publishedAt);
+        binding.detailsDescriptionTextView.setText(article.description);
+        binding.detailsContentTextView.setText(article.content);
+        Picasso.get().load(article.urlToImage).into(binding.detailsImageView);
+    }
+
+}
